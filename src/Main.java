f21ab0a3392b97bf9e9bba1a9f6e30b4fc54e965
Lilnurs <zulpukarov518@icloud.com>
2022-10-08 19:36:24 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        double[] numbers1 = {-1.2, -3.5, 2.2, -15.6, -6.7, 6.6, -3.3, 4.5, -12.1, -23.4, 5.6, -3.8, -5.9, 4.7, -8.8};
+        double average = 0.0;
+        int count = 0;
+        for (double numbers : numbers1) {
+            if (numbers > 0) {
+                count++;
+                average += numbers;
+            }
+
+        }
+        System.out.println(average / count);
+
+    }
+}
